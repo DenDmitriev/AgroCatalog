1289abdb54136acb62d9f11cef99f5c99e472618
DenDmitriev <dv.denstr@gmail.com>
2024-01-19 08:18:58 +0300

    first init

---
+++ AgroCatalog/AgroCatalog/AgroCatalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgroCatalogApp.swift
+//  AgroCatalog
+//
+//  Created by Denis Dmitriev on 19.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct AgroCatalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
